34c080ae5dc533c2b1a7add06fa4846061c52bdc
Aitor Vazquez <aitorvazquezpoza@gmail.com>
2025-09-02 11:07:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-import { Actor } from "apify";
 
-await Actor.main(async () => {
-  console.log("Actor personalizado funcionando 🚀");
-  await Actor.pushData({
-    mensaje: "Hola Normicriano",
-    fecha: new Date().toISOString()
-  });
-});
